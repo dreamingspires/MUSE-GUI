c02a36b17646215d3fc48f96086b0e6a0e7e489b
Szwarc99 <51268010+Szwarc99@users.noreply.github.com>
2022-03-11 05:35:59 +0100

    small fix in global input files

--- muse_gui/backend/pydantic_models/global_input_files_model.py
+++ muse_gui/backend/pydantic_models/global_input_files_model.py
@@ -4,4 +4,4 @@ class GlobalInputFiles(BaseModel):
     # all should be paths to csv files
     projections: str = None
     regions: str = None
-    global_commodities = None
+    global_commodities:str = None
