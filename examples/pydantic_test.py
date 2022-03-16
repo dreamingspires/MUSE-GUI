27aff685f256daed848ad687f014e81c1c33ed27
Szwarc99 <51268010+Szwarc99@users.noreply.github.com>
2022-03-16 19:32:53 +0100

    fixed path in pydantic_test

--- examples/pydantic_test.py
+++ examples/pydantic_test.py
@@ -1,6 +1,6 @@
 import json
 import toml
-from muse.settings_model import SettingsModel
+from muse_gui.backend.settings import SettingsModel
 toml_struct = toml.load('example.toml')
 print(json.dumps(toml_struct, indent=2))
 pydantic_struct = SettingsModel.parse_obj(toml_struct)
