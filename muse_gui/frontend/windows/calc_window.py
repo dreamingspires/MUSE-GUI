eba2f4a9d557a4748b5ed97061a37eaa9090a27f
mark-todd <markpeter.todd@hotmail.co.uk>
2022-03-23 15:51:27 +0000

    Moved each window style in muse_gui windows

---
+++ muse_gui/frontend/windows/calc_window.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from typing import Tuple
+import PySimpleGUI as sg
+    
+
+def boot_waiting_window(font, datastore) -> Tuple[Path,Path]:
+    window = sg.Window(
+        'Waiting', 
+        [[sg.Text('Calculating MUSE')]], 
+        resizable = True,
+        font = font, 
+        auto_size_text=True,
+        finalize=True,
+        element_justification='c'
+    )
+    prices_path, capacity_path = datastore.run_muse()
+    window.close()
+    return prices_path, capacity_path
